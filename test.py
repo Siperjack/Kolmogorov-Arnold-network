9c1ad0dd59a78a0618f117d3d9e2b19884509e34
jstokke <stokke@zib.de>
2024-08-21 14:19:18 +0200

    set up on work computer, imported datasets and added markdown

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import torch
 import numpy as np
+import torchvision.datasets as datasets
 
 A = torch.eye(5)
 print(A)
-B = np.arange(0,10)
+B = np.arange(0, 10)
 print(B)
+mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
+mnist_trainset.data[0:2].type(torch.float32)
+pass
